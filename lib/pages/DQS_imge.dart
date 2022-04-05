2e866146a27ebaf8d82e90b6c2ac08a018db759c
phumipat0824 <086753248909sos@gmail.com>
2022-04-06 05:56:11 +0700

    uplode pdf imge

--- lib/pages/DQS_imge.dart
+++ lib/pages/DQS_imge.dart
@@ -1,5 +1,5 @@
+import 'package:dqs_mobileapp/pages/DQS_create_qrcode.dart';
 import 'package:dqs_mobileapp/pages/DQS_home.dart';
-import 'package:dqs_mobileapp/pages/DQS_imge.dart';
 import 'package:dqs_mobileapp/pages/DQS_login.dart';
 import 'package:dqs_mobileapp/pages/DQS_pdf.dart';
 import 'package:dqs_mobileapp/pages/DQS_show_qrcode.dart';
@@ -7,15 +7,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:go_router/go_router.dart';
+import 'package:dio/dio.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:path/path.dart';
 
-class DQS_create_qrcode extends StatefulWidget {
-  const DQS_create_qrcode({Key? key}) : super(key: key);
+class DQS_imge extends StatefulWidget {
+  const DQS_imge({Key? key}) : super(key: key);
 
   @override
-  State<DQS_create_qrcode> createState() => _DQS_create_qrcodeState();
+  State<DQS_imge> createState() => _DQS_imgeState();
 }
 
-class _DQS_create_qrcodeState extends State<DQS_create_qrcode> {
+class _DQS_imgeState extends State<DQS_imge> {
   TextEditingController url = TextEditingController();
   String? txtUsername = ' ';
   @override
@@ -53,7 +56,14 @@ class _DQS_create_qrcodeState extends State<DQS_create_qrcode> {
                             Row(children: <Widget>[
                               SizedBox(width: 10),
                               ElevatedButton(
-                                onPressed: () {},
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const DQS_create_qrcode()),
+                                  );
+                                },
                                 child: Text("เว็บไซต์"),
                                 style: ButtonStyle(
                                     backgroundColor: MaterialStateProperty.all(
@@ -102,12 +112,6 @@ class _DQS_create_qrcodeState extends State<DQS_create_qrcode> {
                                     );
                                   } else {
                                     print('${txtUsername}');
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) =>
-                                              const DQS_imge()),
-                                    );
                                   }
                                 },
                                 child: Text("รูปภาพ"),
@@ -119,13 +123,24 @@ class _DQS_create_qrcodeState extends State<DQS_create_qrcode> {
                               ),
                             ]),
                             SizedBox(height: 20),
-                            Container(
-                              width: 350.0,
-                              child: TextField(
-                                  controller: url,
-                                  decoration: InputDecoration(
-                                      labelText: 'เว็บไซต์',
-                                      border: OutlineInputBorder())),
+                            ElevatedButton(
+                              onPressed: () async {
+                                FilePickerResult? result =
+                                    await FilePicker.platform.pickFiles(
+                                  type: FileType.custom,
+                                  allowedExtensions: ['jpg'],
+                                );
+                                // FilePickerResult? result = await FilePicker.platform.pickFiles();
+                                if (result == null) {
+                                  print("No file selected");
+                                } else {
+                                  String? path = result.files.single.path;
+                                  _upload(result, path);
+                                  print(result.files.single.name);
+                                  print(result.files.single.path);
+                                }
+                              },
+                              child: Text("อัพโหลดรูปภาพ"),
                             ),
                             // SizedBox(height: 20),
 
@@ -175,6 +190,15 @@ class _DQS_create_qrcodeState extends State<DQS_create_qrcode> {
     );
   }
 
+  final GoRouter _router = GoRouter(
+    routes: <GoRoute>[
+      GoRoute(
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) =>
+            const login_page(),
+      ),
+    ],
+  );
   void getUsername() async {
     final SharedPreferences pref = await SharedPreferences.getInstance();
     setState(() {
@@ -186,4 +210,23 @@ class _DQS_create_qrcodeState extends State<DQS_create_qrcode> {
 Future<void> setURL(textURL) async {
   final SharedPreferences pref = await SharedPreferences.getInstance();
   pref.setString('url', textURL);
+}
+
+void _upload(FilePickerResult result, String? path) async {
+  String fileName = result.files.single.name;
+  String filePath = './' + result.files.single.name;
+
+  FormData data = FormData.fromMap({
+    "file": await MultipartFile.fromFile(
+      path!,
+      filename: fileName,
+    ),
+  });
+
+  Dio dio = new Dio();
+  dio
+      .post("http://103.129.15.182/DQS/assets/user/DQS_api_upload.php",
+          data: data)
+      .then((response) => print(response))
+      .catchError((error) => print(error));
 }
--- lib/pages/DQS_pdf.dart
+++ lib/pages/DQS_pdf.dart
@@ -1,27 +1,32 @@
+import 'package:dqs_mobileapp/pages/DQS_create_qrcode.dart';
 import 'package:dqs_mobileapp/pages/DQS_home.dart';
 import 'package:dqs_mobileapp/pages/DQS_imge.dart';
 import 'package:dqs_mobileapp/pages/DQS_login.dart';
-import 'package:dqs_mobileapp/pages/DQS_pdf.dart';
 import 'package:dqs_mobileapp/pages/DQS_show_qrcode.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:go_router/go_router.dart';
+import 'package:dio/dio.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:path/path.dart';
 
-class DQS_create_qrcode extends StatefulWidget {
-  const DQS_create_qrcode({Key? key}) : super(key: key);
+class DQS_pdf extends StatefulWidget {
+  const DQS_pdf({Key? key}) : super(key: key);
 
   @override
-  State<DQS_create_qrcode> createState() => _DQS_create_qrcodeState();
+  State<DQS_pdf> createState() => _DQS_pdfState();
 }
 
-class _DQS_create_qrcodeState extends State<DQS_create_qrcode> {
+class _DQS_pdfState extends State<DQS_pdf> {
   TextEditingController url = TextEditingController();
   String? txtUsername = ' ';
+  String? txtUrl = ' ';
   @override
   void initState() {
     super.initState();
     getUsername();
+    getUrl();
   }
 
   @override
@@ -53,7 +58,14 @@ class _DQS_create_qrcodeState extends State<DQS_create_qrcode> {
                             Row(children: <Widget>[
                               SizedBox(width: 10),
                               ElevatedButton(
-                                onPressed: () {},
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const DQS_create_qrcode()),
+                                  );
+                                },
                                 child: Text("เว็บไซต์"),
                                 style: ButtonStyle(
                                     backgroundColor: MaterialStateProperty.all(
@@ -119,13 +131,30 @@ class _DQS_create_qrcodeState extends State<DQS_create_qrcode> {
                               ),
                             ]),
                             SizedBox(height: 20),
-                            Container(
-                              width: 350.0,
-                              child: TextField(
-                                  controller: url,
-                                  decoration: InputDecoration(
-                                      labelText: 'เว็บไซต์',
-                                      border: OutlineInputBorder())),
+                            ElevatedButton(
+                              onPressed: () async {
+                                FilePickerResult? result =
+                                    await FilePicker.platform.pickFiles(
+                                  type: FileType.custom,
+                                  allowedExtensions: ['pdf'],
+                                );
+                                // FilePickerResult? result = await FilePicker.platform.pickFiles();
+                                if (result == null) {
+                                  print("No file selected");
+                                } else {
+                                  String? path = result.files.single.path;
+                                  _upload(result, path);
+                                  //setUrl(result.files.single.name);
+                                  txtUrl =
+                                      "http://103.129.15.182/DQS/assets/user/" +
+                                          '${txtUsername}' +
+                                          "/Home/" +
+                                          result.files.single.name;
+                                  print(result.files.single.name);
+                                  print(result.files.single.path);
+                                }
+                              },
+                              child: Text("อัพโหลดไฟล์"),
                             ),
                             // SizedBox(height: 20),
 
@@ -142,7 +171,7 @@ class _DQS_create_qrcodeState extends State<DQS_create_qrcode> {
                                 var homeRounte = new MaterialPageRoute(
                                   builder: (BuildContext contex) =>
                                       DQS_show_qrcode(
-                                    value_url: url.text,
+                                    value_url: '${txtUrl}',
                                   ),
                                 );
                                 Navigator.of(context).push(homeRounte);
@@ -175,10 +204,31 @@ class _DQS_create_qrcodeState extends State<DQS_create_qrcode> {
     );
   }
 
+  final GoRouter _router = GoRouter(
+    routes: <GoRoute>[
+      GoRoute(
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) =>
+            const login_page(),
+      ),
+    ],
+  );
   void getUsername() async {
     final SharedPreferences pref = await SharedPreferences.getInstance();
     setState(() {
       txtUsername = pref.getString('username');
+    });
+  }
+
+  Future<void> setUrl(textUrl) async {
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    pref.setString('url', textUrl);
+  }
+
+  void getUrl() async {
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    setState(() {
+      txtUrl = pref.getString('url');
     });
   }
 }
@@ -186,4 +236,23 @@ class _DQS_create_qrcodeState extends State<DQS_create_qrcode> {
 Future<void> setURL(textURL) async {
   final SharedPreferences pref = await SharedPreferences.getInstance();
   pref.setString('url', textURL);
+}
+
+void _upload(FilePickerResult result, String? path) async {
+  String fileName = result.files.single.name;
+  String filePath = './' + result.files.single.name;
+
+  FormData data = FormData.fromMap({
+    "file": await MultipartFile.fromFile(
+      path!,
+      filename: fileName,
+    ),
+  });
+
+  Dio dio = new Dio();
+  dio
+      .post("http://103.129.15.182/DQS/assets/user/DQS_api_upload.php",
+          data: data)
+      .then((response) => print(response))
+      .catchError((error) => print(error));
 }
